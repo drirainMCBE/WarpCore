a4b247ec66a3f5a09acea380091db9496a88d320
bluefirewolf534 <bluefirewolf534@gmail.com>
2021-07-29 22:06:41 +0900

    :white_check_mark: add manageWarpCommand

---
+++ src/RoMo/WarpCore/command/manageWarpCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RoMo\WarpCore\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use RoMo\WarpCore\WarpCore;
+
+class manageWarpCommand extends Command{
+    public function __construct(){
+        $cmd = WarpCore::getCmd("manage.warp");
+        parent::__construct($cmd["name"], $cmd["description"], $cmd["usageMessage"], $cmd["aliases"]);
+        $this->setPermission("manage-warp");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(!$this->testPermission($sender)){
+            return;
+        }
+        if(!$sender instanceof Player){
+            $sender->sendMessage(WarpCore::getMessage("must.do.in.game"));
+            return;
+        }
+        //TODO
+    }
+}
