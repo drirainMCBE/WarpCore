4027c47b171b95bc6bdf0984bcd0cb1365b642aa
bluefirewolf534 <bluefirewolf534@gmail.com>
2021-07-29 22:09:24 +0900

    :white_check_mark: add regist command AND init message data

--- src/RoMo/WarpCore/WarpCore.php
+++ src/RoMo/WarpCore/WarpCore.php
@@ -4,6 +4,7 @@ namespace RoMo\WarpCore;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
+use RoMo\WarpCore\command\manageWarpCommand;
 use RoMo\WarpCore\lib\translateTrait;
 
 class WarpCore extends PluginBase{
@@ -12,6 +13,7 @@ class WarpCore extends PluginBase{
     use translateTrait;
 
     public function onEnable() : void{
-        //TODO
+        self::initMessage("kor");
+        $this->getServer()->getCommandMap()->register("WarpCore", new manageWarpCommand());
     }
 }
